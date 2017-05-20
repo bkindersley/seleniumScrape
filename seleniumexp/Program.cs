14ed814a78b85d03ee0b509d0f2a64c79d6e6ccc
Bria Kindersley <bria.kindersley@trulioo.com>
2017-05-19 17:15:25 -0700

    httpclient stuff

--- seleniumexp/Program.cs
+++ seleniumexp/Program.cs
@@ -5,13 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
 using System.Diagnostics;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Remote;
-using OpenQA.Selenium.Support.UI;
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.IE;
-using OpenQA.Selenium.Interactions;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
+
 namespace SeleniumEsperiments
 {
     class Program
@@ -19,126 +19,195 @@ namespace SeleniumEsperiments
         static void Main(string[] args)
         {
 
-            string panelText = "Change NSW Driver Licence, NSW Photo Card and vehicle registration contact details";
+            string csrfJsonString = GetCSRFJson();
 
-            IWebDriver driver = new ChromeDriver();
-            WebDriverWait w = new WebDriverWait(new SystemClock(), driver, TimeSpan.FromSeconds(10), TimeSpan.FromMilliseconds(5));
-            driver.Navigate().GoToUrl("https://my.service.nsw.gov.au/MyServiceNSW/index#/rms/changeDetails");
-            //on page with radio buttons
-            w.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.ClassName("panel-group")));
-            IWebElement panelGroup = driver.FindElement(By.ClassName("panel-group"));
-            IWebElement form = driver.FindElement(By.ClassName("form"));
+            VFCSRFObj csrfJsonObj = JsonConvert.DeserializeObject<VFCSRFObj>(csrfJsonString, new CSRFConverter());
 
-            IList<IWebElement> panels = new List<IWebElement>();
-            int i = 0;
-            while (!string.Equals(panels.FirstOrDefault()?.Text, panelText) && i < 10)
+            Debug.Write("done");
+
+        }
+
+        static async Task Send()
+        {
+            string url = "https://my.service.nsw.gov.au/MyServiceNSW/apexremote";
+
+            HttpClient client = new HttpClient();
+
+            Dictionary<string, string> reqStrings = new Dictionary<string, string>
             {
-                panels = panelGroup.FindElements(By.ClassName("panel"));
-                i++;
-            }
-            Debug.WriteLine(i);
-            if (!string.Equals(panels.FirstOrDefault()?.Text, panelText))
-            {
-                Debug.WriteLine("panel not found");
-                return;
-            }
+                { "createMaDashboardTransaction","{ 'action':'maDashboardCtrl','method':'createMaDashboardTransaction','data':['1172fa7c7be06f6f17525c96d74da5855007cfa4ce9edde7cf4f5df0c367c536',''],'type':'rpc','tid':22,'ctx':{'csrf':'VmpFPSxNakF4Tnkwd05TMHlNbFF5TURveE1Ub3hNUzQ1TWpCYSxXb21JLVVzdUJFY2hKWHlwQ2ZDVFhLLFpEQXpZV1E1','vid':'06690000005lUNp','ns':'','ver':34}}" },
+                { "createChangeAddressTransaction","{'action':'RMSTransactionService','method':'createChangeAddressTransaction','data':['1172fa7c7be06f6f17525c96d74da5855007cfa4ce9edde7cf4f5df0c367c536',''],'type':'rpc','tid':24,'ctx':{'csrf':'VmpFPSxNakF4Tnkwd05TMHlNbFF5TURveE1Ub3hNUzQ1TWpaYSx0TjVtTmVXSVFkWUtVMEE1ajJpWWZVLE9EWmtZMlZq','vid':'06690000005lUNp','ns':'','ver':34}}" },
+                { "validateLicenseDetails", "{'action':'RMSTransactionService','method':'validateLicenseDetails','data':['###','!!!','$$$','%%%'],'type':'rpc','tid':25,'ctx':{'csrf':'VmpFPSxNakF4Tnkwd05TMHlNbFF5TURveE1Ub3hNUzQ1TWpkYSxNazU3RGtpeVpUaG5rREJ2YnU4WlVmLE5EVmtNVFpr','vid':'06690000005lUNp','ns':'','ver':34}}" }
 
-            IWebElement panel = panels.First();
-            panel.Click();
-
-            IWebElement submitBtn = form.FindElement(By.TagName("input"));
-            submitBtn.Click();
+            };
             
-            w.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.XPath("/html/body//p/button")));
+            var request = new HttpRequestMessage(HttpMethod.Post, new Uri(url)) { Content = new StringContent(reqStrings["createChangeAddressTransaction"], Encoding.UTF8, "application/json") };
+            request.Headers.Add("X-User-Agent", "Visualforce-Remoting");
+            request.Headers.Add("Origin", "https://my.service.nsw.gov.au");
+            request.Headers.Add("Referer","https://my.service.nsw.gov.au/MyServiceNSW/index");
+            request.Headers.Add("Host" ,"my.service.nsw.gov.au");
 
-            IList<IWebElement> buttons = driver.FindElements(By.XPath("/html/body//p/button"));
-
-            IWebElement guestLoginBtn = null;
-
-            foreach (IWebElement btn in buttons)
+            var resp = await client.SendAsync(request);
+            object product;
+            List<ApexResultObj> res = null;
+            if (resp.IsSuccessStatusCode)
             {
-                if (string.Equals(btn.Text, "Continue as guest"))
+                product = await resp.Content.ReadAsAsync<object>();
+                
+                res = JsonConvert.DeserializeObject<List<ApexResultObj>>(product.ToString());
+
+                string valueToRecord = res.FirstOrDefault()?.result.statusObject;
+
+                request = new HttpRequestMessage(HttpMethod.Post, new Uri(url)) { Content = new StringContent(reqStrings["validateLicenseDetails"].Replace("###", valueToRecord).Replace("!!!", "Smith").Replace("$$$", "111111").Replace("%%%", "1111111"), Encoding.UTF8, "application/json") };
+                request.Headers.Add("X-User-Agent", "Visualforce-Remoting");
+                request.Headers.Add("Origin", "https://my.service.nsw.gov.au");
+                request.Headers.Add("Referer", "https://my.service.nsw.gov.au/MyServiceNSW/index");
+                request.Headers.Add("Host", "my.service.nsw.gov.au");
+
+                resp = await client.SendAsync(request);
+                if (resp.IsSuccessStatusCode)
                 {
-                    guestLoginBtn = btn;
-                    break;
+                    product = await resp.Content.ReadAsAsync<object>();
                 }
             }
 
-            if (guestLoginBtn == null)
+            Debug.WriteLine("done");
+        }
+
+        static string GetCSRFJson()
+        {
+            //HttpClient client = new HttpClient();
+            //var request = new HttpRequestMessage(HttpMethod.Get, new Uri("https://my.service.nsw.gov.au/MyServiceNSW/index"));
+            //var resp = await client.SendAsync(request);
+            //object product;
+            //if (resp.IsSuccessStatusCode)
+            //{
+            //    product = await resp.Content.ReadAsAsync<object>();
+
+            //}
+            string htmlCode = "";
+            string jsonString = "";
+            using (WebClient client = new WebClient())
             {
-                Debug.WriteLine("guest login not found");
-                return;
+                htmlCode = client.DownloadString("https://my.service.nsw.gov.au/MyServiceNSW/index");
             }
 
-            guestLoginBtn.Click();
-
-            w.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.ClassName("panel")));
-
-            IWebElement textPanel = driver.FindElement(By.ClassName("panel"));
-
-            w.Until(d => textPanel.FindElements(By.XPath(".//form")).Count() > 1);
-
-            IList<IWebElement> forms = textPanel.FindElements(By.XPath(".//form"));
-
-            IWebElement detailsForm = null;
-
-            foreach (IWebElement frm in forms)
+            if (!string.IsNullOrEmpty(htmlCode))
             {
-                IList<IWebElement> ins = frm.FindElements(By.ClassName("form-control"));
-                if (ins.Count() == 3)
+                string pattern = @"\bVisualforce\b(.*);";
+                Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+                MatchCollection matches = rgx.Matches(htmlCode);
+
+                List<string> matchTexts = new List<string>();
+                foreach (Match m in matches)
                 {
-                    detailsForm = frm;
-                    break;
-                }             
-            }
+                    matchTexts.Add(m.Value);
+                }
 
-            if (detailsForm == null)
-            {
-                Debug.WriteLine("form not found");
-            }
+                if (matchTexts.Count() > 1)
+                {
+                    Debug.WriteLine("regex ambiguity");
+                    return jsonString;
+                }
 
-            IWebElement lName = detailsForm.FindElement(By.XPath(".//input[contains(@id, 'lastName')]"));
-            IWebElement phIDNum = detailsForm.FindElement(By.XPath(".//input[contains(@id, 'photoIdNumber')]"));
-            IWebElement dlNum = detailsForm.FindElement(By.XPath(".//input[contains(@id, 'cardNumber')]"));
+                string matchedText = matchTexts.FirstOrDefault();
 
-            lName.SendKeys("Smith");
-            phIDNum.SendKeys("11111111");
-            dlNum.SendKeys("11111111");
+                int firstIndex = matchedText.IndexOf("{");
+                int lastIndex = matchedText.LastIndexOf("}");
 
-            IWebElement nextBtn = detailsForm.FindElement(By.XPath(".//input[@value = 'Next']"));
+                if (firstIndex > -1 && lastIndex > firstIndex)
+                {
+                    jsonString = matchedText.Substring(firstIndex, lastIndex - firstIndex + 1);
+                }
+                else
+                {
+                    Debug.WriteLine("bad bracket matching");
+                    return jsonString;
+                }
 
-            nextBtn.Click();
-
-            IWebElement notFoundAlert = null;
-
-            try
-            {
-                w.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.ClassName("alert-dismissible")));
-                notFoundAlert = textPanel.FindElement(By.ClassName("alert-dismissible"));
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex.Message);
             }
             
-            if (notFoundAlert == null)
+            return jsonString;
+        }
+    }
+
+    class ApexResultObj
+    {
+        public string action = "RMSTransactionService";
+        public ApexResult result;
+        public string statusCode;
+        public string method = "hasRMSEmail";
+        public int tid;
+        public string type = "rpc";
+
+
+    }
+
+    class ApexResult
+    {
+        public string statusCode;
+        public string statusMessage;
+        public string statusObject;
+    }
+
+    class VFCSRFObj
+    {
+        public Dictionary<string, string> vf { get; set; }
+        public Dictionary<string, Dictionary<string, string>> actions { get; set; }
+        public Dictionary<string, string> service { get; set; }
+    }
+
+    public class CSRFConverter : JsonConverter
+     {
+ 
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+        
+        }
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
             {
-                if (string.Equals(driver.Url, "https://my.service.nsw.gov.au/MyServiceNSW/index#/rms/changeDetails/address"))
+                JObject item = JObject.Load(reader);
+
+                if (item["vf"] != null && item["actions"] != null && item["service"] != null)
                 {
-                    Debug.WriteLine("found match?");
+                    Dictionary<string, string> vf = item["vf"].ToObject<Dictionary<string, string>>(serializer);
+                    Dictionary<string, string> service = item["service"].ToObject<Dictionary<string, string>>(serializer);
 
+                    Dictionary<string, Dictionary<string, string>> actions = new Dictionary<string, Dictionary<string, string>>();
+
+                    JProperty actionProp = item.Properties().Where(p => p.Name == "actions").First();
+
+                    JObject actn = (JObject)actionProp.Value;
+
+                    foreach ( JProperty prop in actn.Properties())
+                    {
+                        if (prop.Value["ms"] != null)
+                        {
+                            actions.Add(prop.Name, prop.Value["ms"].ToObject<Dictionary<string, string>>(serializer));
+                        }
+                    }
+
+                    VFCSRFObj ret = new VFCSRFObj();
+                    ret.actions = actions;
+                    ret.vf = vf;
+                    ret.service = service;
+
+                    return ret;
                 }
-            }
-            else if (notFoundAlert.Text.Contains("do not match"))
-            {
-                Debug.WriteLine("No Match!");
-            }
-            else
-            {
-                Debug.WriteLine("Not the expected error!");
+
+                // This should not happen. Perhaps better to throw exception at this point?
+                return null;
             }
 
-            driver.Quit();
+            public override bool CanRead
+            {
+                get { return false; }
+            }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
         }
     }
 }
